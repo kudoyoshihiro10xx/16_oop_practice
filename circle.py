59b4a5af57453bb28f9eef81f39ae49c0e17696e
Kudo <kudo@example.com>
2019-03-11 12:08:08 +0900

    半径と円周の計算

---
+++ circle.py
@@ -0,0 +1,33 @@
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        # 円の面積 = 半径 * 半径 * 円周率
+        return (self.radius ** 2) * 3.14
+
+    def circumference(self):
+        return 2 * self.radius * 3.14
+
+
+if __name__ == '__main__':
+    # 半径が 1 の円
+    circle1 = Circle(radius=3)  # インスタンス化
+
+    # circle1の面積を求める
+    print(circle1.area())
+
+    # circle2の面積を求める
+    circle2 = Circle(20)
+    print(circle2.area())
+
+    # ユーザーから、半径の長さを入力して受け取り、その面積を出力する
+
+    data = int(input("半径を入力して下さい: "))
+    circle3 = Circle(data)
+    print(circle3.area())
+
+    # 円周を求める
+    print(circle3.circumference())
+
+    print(circle1.radius)
--- rectangle.py
+++ rectangle.py
@@ -1,9 +1,16 @@
 class Rectangle:
-    def __init__(self):
-        print("Called! __init__")
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
 
 
 if __name__ == '__main__':
-    r1 = Rectangle()
+    # 横: 4 縦: 3 の長方形
+    r1 = Rectangle(4, 3)  # インスタンス化
 
-    r2 = Rectangle()
+    # 3分課題:
+    area1 = r1.area()  # 面積を計算する
+    print(area1)  # 12が出力される
