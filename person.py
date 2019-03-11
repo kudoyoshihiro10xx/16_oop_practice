71adf908e4898c58916d72172855069b7df6fcaf
Kudo <kudo@example.com>
2019-03-11 09:09:50 +0900

    Person class を実装

---
+++ person.py
@@ -0,0 +1,19 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def profile(self):
+        return f"Name: {self.name}, Age: {self.age}"
+
+
+if __name__ == '__main__':
+    # 'クラス'を使うイメージ
+    bob = Person("Bob", 15)
+    print(bob.profile())  # 'Name: Bob, Age: 15'と出力されて欲しい
+
+    tom = Person("Tom", 24)
+    print(tom.profile())  # 'Name: Tom, age: 24'と出力されて欲しい
+
+    ken = Person("Ken", 42)
+    print(ken.profile())
