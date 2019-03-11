7b9fe96798a1e1e26857c4233ba414343fd0287a
Kudo <kudo@example.com>
2019-03-11 09:59:21 +0900

    イニシャライズとインスタンス化を覚えた

--- person.py
+++ person.py
@@ -9,11 +9,11 @@ class Person:
 
 if __name__ == '__main__':
     # 'クラス'を使うイメージ
-    bob = Person("Bob", 15)
+    bob = Person("Bob", 15)  # インスタンス化
     print(bob.profile())  # 'Name: Bob, Age: 15'と出力されて欲しい
 
-    tom = Person("Tom", 24)
+    tom = Person("Tom", 24)  #インスタンス化
     print(tom.profile())  # 'Name: Tom, age: 24'と出力されて欲しい
 
-    ken = Person("Ken", 42)
+    ken = Person("Ken", 42)  #インスタンス化
     print(ken.profile())
